18789cf00e4767588a3ee6319253c316e32a2d2e
medilies <medilies@gmail.com>
2020-12-14 22:43:10 +0100

    test heroku postgresql

--- index.php
+++ index.php
@@ -1,2 +1,41 @@
 <?php
-require_once"./index.html";
+// add PDO that saves visitors in a DB to replace heroku paid metric
+
+// $dbopts = parse_url(getenv('DATABASE_URL'));
+// $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+//     array(
+//         'pdo.server' => array(
+//             'driver' => 'pgsql',
+//             'user' => $dbopts["user"],
+//             'password' => $dbopts["pass"],
+//             'host' => $dbopts["host"],
+//             'port' => $dbopts["port"],
+//             'dbname' => ltrim($dbopts["path"], '/'),
+//         ),
+//     )
+// );
+
+// $st = $app['pdo']->prepare('SELECT timestamp FROM visitors');
+// $st->execute();
+
+// $names = array();
+// while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+//     $names[] = $row;
+// }
+
+$db = parse_url(getenv("DATABASE_URL"));
+
+$pdo = new PDO("pgsql:" . sprintf(
+    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+    $db["host"],
+    $db["port"],
+    $db["user"],
+    $db["pass"],
+    ltrim($db["path"], "/")
+));
+
+var_dump($pdo);
+
+// INSERT INTO `visitors` (`moment`, `ip`) VALUES (current_timestamp(), '192.168.1.2');
+
+require_once "./index.html";
