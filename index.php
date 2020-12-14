a140a9085c2102637be5bbc669db96ee2bee2bae
medilies <medilies@gmail.com>
2020-12-14 22:51:01 +0100

    test heroku postgresql 3

--- index.php
+++ index.php
@@ -1,28 +1,6 @@
 <?php
 // add PDO that saves visitors in a DB to replace heroku paid metric
 
-// $dbopts = parse_url(getenv('DATABASE_URL'));
-// $app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-//     array(
-//         'pdo.server' => array(
-//             'driver' => 'pgsql',
-//             'user' => $dbopts["user"],
-//             'password' => $dbopts["pass"],
-//             'host' => $dbopts["host"],
-//             'port' => $dbopts["port"],
-//             'dbname' => ltrim($dbopts["path"], '/'),
-//         ),
-//     )
-// );
-
-// $st = $app['pdo']->prepare('SELECT timestamp FROM visitors');
-// $st->execute();
-
-// $names = array();
-// while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
-//     $names[] = $row;
-// }
-
 $db = parse_url(getenv("DATABASE_URL"));
 
 $pdo = new PDO("pgsql:" . sprintf(
