c29efd21da876ee5f88f3a822c3709e08b273763
medilies <medilies@gmail.com>
2020-12-14 22:56:56 +0100

    test heroku postgresql 4 (insert)

--- index.php
+++ index.php
@@ -3,17 +3,30 @@
 
 $db = parse_url(getenv("DATABASE_URL"));
 
-$pdo = new PDO("pgsql:" . sprintf(
-    "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-    $db["host"],
-    $db["port"],
-    $db["user"],
-    $db["pass"],
-    ltrim($db["path"], "/")
-));
+echo $_SERVER['REMOTE_ADDR'];
+try {
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+    $query = 'INSERT INTO visitors(address) VALUES(:add,)';
+
+    $query = $pdo->prepare($query);
+
+    $query->bindParam(':add', $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
+    $query->execute();
+
+} catch (PDOException $e) {
+    echo "Error!: " . $e->getMessage() . "<br/>";
+}
 
 $data = $pdo->query("SELECT * FROM visitors")->fetch();
-var_dump($data);
+print_r($data);
 
 // INSERT INTO `visitors` (`moment`, `ip`) VALUES (current_timestamp(), '192.168.1.2');
 
